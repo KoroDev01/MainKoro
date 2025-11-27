63271bf19f73ee95845efaf57c4db223ff79ea11
KoroDev01 <malifaiz03@gmail.com>
2025-11-27 14:36:46 +0100

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./Components/About";
 import Skills from "./Components/Skills";
 import Projects from "./Components/Projects";
