ccbd4113e90cc8d62403396852232683dc2317fb
KoroDev01 <malifaiz03@gmail.com>
2026-07-05 15:26:32 +0200

    s

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// Production : https://korodev01.github.io/MainKoro/
 export default defineConfig(({ command }) => ({
   plugins: [tailwindcss(), react()],
   base: command === "build" ? "/MainKoro/" : "/",
